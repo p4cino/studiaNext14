4c18c8080cb7dbbe4ee96e5b796462b280e61a33
wpuzio <wpuzio@estee.com>
2023-11-18 22:44:29 +0100

    feat(login) add

---
+++ src/app/components/LoginModal/LoginModal.tsx
@@ -0,0 +1,113 @@
+'use client';
+import { Fragment, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
+
+interface ILoginModal {
+  popupOpenState?: boolean;
+}
+
+export default function LoginModal({ popupOpenState = false }: ILoginModal = {}) {
+  const [open, setOpen] = useState(popupOpenState);
+
+  const emailInputRef = useRef(null);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={emailInputRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full w-full">
+                    <UserCircleIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <div className="mt-3 text-center w-full">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-base font-semibold leading-6 text-gray-900"
+                    >
+                      Login
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <form className="space-y-6" action="#" method="POST">
+                        <div>
+                          <label
+                            htmlFor="email"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Email address
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              id="email"
+                              name="email"
+                              type="email"
+                              autoComplete="email"
+                              required
+                              ref={emailInputRef}
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="flex items-center justify-between">
+                            <label
+                              htmlFor="password"
+                              className="block text-sm font-medium leading-6 text-gray-900"
+                            >
+                              Password
+                            </label>
+                          </div>
+                          <div className="mt-2">
+                            <input
+                              id="password"
+                              name="password"
+                              type="password"
+                              autoComplete="current-password"
+                              required
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                          >
+                            Sign in
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
--- src/app/components/StickyNavbar/StickyNavbar.tsx
+++ src/app/components/StickyNavbar/StickyNavbar.tsx
@@ -2,7 +2,10 @@
 import React from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { Button, IconButton, MobileNav, Navbar, Typography } from '@material-tailwind/react';
+
+import LoginModal from '@/components/LoginModal/LoginModal';
 
 type MenuItem = {
   name: string;
@@ -42,6 +45,7 @@ const MenuButtonList: MenuItem = [
 
 export function StickyNavbar() {
   const [openNav, setOpenNav] = React.useState(false);
+  const [popupVisible, setPopupVisible] = React.useState(false);
 
   React.useEffect(() => {
     window.addEventListener('resize', () => window.innerWidth >= 960 && setOpenNav(false));
@@ -66,61 +70,66 @@ export function StickyNavbar() {
   );
 
   return (
-    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-      <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography as="a" href="/" className="mr-4 cursor-pointer py-1.5 font-medium">
-          <Image src="/next.svg" alt={''} width={100} height={20} /> Material Tailwind
-        </Typography>
-        <div className="flex items-center gap-4">
-          <div className="mr-4 hidden lg:block">{navList}</div>
+    <>
+      <LoginModal />
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography as="a" href="/" className="mr-4 cursor-pointer py-1.5 font-medium">
+            <Image src="/next.svg" alt={''} width={100} height={20} /> Material Tailwind
+          </Typography>
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <div className="flex items-center gap-x-1">
+              {MenuButtonList.map((item) => (
+                <Button variant="text" size="sm" className="hidden lg:inline-block" key={item.href}>
+                  <span>{item.name}</span>
+                </Button>
+              ))}
+            </div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+        <MobileNav open={openNav}>
+          {navList}
           <div className="flex items-center gap-x-1">
             {MenuButtonList.map((item) => (
-              <Button variant="text" size="sm" className="hidden lg:inline-block" key={item.href}>
-                <span>{item.name}</span>
-              </Button>
+              <Link href={item.href}>
+                <Button fullWidth variant="text" size="sm" className="" key={item.href}>
+                  <span>{item.name}</span>
+                </Button>
+              </Link>
             ))}
           </div>
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            )}
-          </IconButton>
-        </div>
-      </div>
-      <MobileNav open={openNav}>
-        {navList}
-        <div className="flex items-center gap-x-1">
-          {MenuButtonList.map((item) => (
-            <Button fullWidth variant="text" size="sm" className="" key={item.href}>
-              <span>{item.name}</span>
-            </Button>
-          ))}
-        </div>
-      </MobileNav>
-    </Navbar>
+        </MobileNav>
+      </Navbar>
+    </>
   );
 }
