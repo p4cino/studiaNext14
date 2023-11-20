23ade4b44bb726ba088e628decb8430beb492fac
p4cino <wojciechpuzio@hotmail.com>
2023-11-20 20:57:40 +0100

    fix(footer) delete unnecessary comment

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,8 +2,6 @@ import { FunctionComponent } from 'react';
 
 import { Typography } from '@material-tailwind/react';
 
-//const LoginModal: FunctionComponent<ILoginModal> = ({ popupStateHandler, popupOpenState }) => {
-
 const Footer: FunctionComponent<{}> = ({}) => {
   return (
     <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
